fead1e394614f44f03b21a54f1f83316a64d17c9
Gilbert Krantz <wilbertchandra.official@gmail.com>
2024-08-06 15:23:58 +0700

    fix: vite-config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy/',
   define: {
     "process.env.IS_PREACT": JSON.stringify("true"),
 },
