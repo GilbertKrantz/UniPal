cbd9a679686804c5566eed1ddaa4e20c23ae9496
ElTheHuman <felixcristiano102938@gmail.com>
2024-08-09 21:40:16 +0700

    Deployment attempt

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/UniPal/",
+  base:"/",
   plugins: [react()],
   define: {
     "process.env.IS_PREACT": JSON.stringify("true"),
